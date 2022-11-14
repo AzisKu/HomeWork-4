0afce7f660f6a6478d0f82318b129db3c23a8246
AzisKu <lanfix.cod@gmail.com>
2022-11-14 19:55:09 +0600

    4homework

---
+++ lesson3.js
@@ -0,0 +1,37 @@
+function showMessage() {
+    let message = "Привет,я JavaScript"; // локальная переменная
+    alert(message)
+}
+showMessage(); //Привет,я JavaScript
+
+// (function() {
+//     alert('Я буду найстойчив и выучу все возможные записи функций!');
+// })();
+//
+// (() => {
+//     alert('JavaScript - это мултипарадигменный язык програмирования.');
+// })();
+//
+// (function() {
+//     alert('Твоя страница была взломана,все твои данные наши >:)');
+// })();
+
+function first(){
+    console.log("hello");
+}
+function second(){
+    console.log("JavaScript");
+}
+function third(){
+    console.log("!");
+}
+first("");
+second("");
+
+
+
+
+
+
+
+
